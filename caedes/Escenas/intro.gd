1061005761f863cb9d2c74e8ac3cc873b5e79064
aaleex05 <alexapsa2007@gmail.com>
2025-03-17 20:28:14 +0100

    doorPad

--- caedes/Escenas/intro.gd
+++ caedes/Escenas/intro.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+@onready var colisionPuerta = $Area2D/CollisionShape2D
+@onready var doorPad = $Area2D/Keypad
+@onready var labelPad: Script = load("res://assets/props/panel.gd")
+
+@onready var puertaAbierta: Sprite2D = $Abierta
+@onready var puertaCerrada: Sprite2D = $Cerrada
+
 @onready var andres: CharacterBody2D = $Andres
 @onready var enemy: CharacterBody2D = $Enemy
 @onready var intro: Sprite2D = $Intro
 @onready var intro_sin: Sprite2D = $IntroSin
 @onready var andresScript: Script = load("res://assets/character/andres/characterAndresWASD.gd")
 @onready var enemyScript: Script = load("res://assets/enemies/enemy.gd")
+
 func _ready():
 	andres.set_script(null)
 	enemy.set_script(null)
@@ -20,5 +28,25 @@ func _physics_process(delta):
 		andres.set_script(andresScript)
 		enemy.set_script(enemyScript)
 		intro_sin.visible = true
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if Input.is_action_pressed("interactuar"):
+		colisionPuerta.show()
+		doorPad.visible = true
+		andres.set_script(null)
 		
+		if global.labelOK == true:
+			puertaCerrada.visible = true
+			puertaAbierta.visible = false
+		else:
+			puertaCerrada.visible = false
+			puertaAbierta.visible = true
 		
+		if global.cerrarPad == true:
+			andres.set_script(andresScript)
+		else:
+			andres.set_script(null)
+		
+	
+
+	
--- caedes/global.gd
+++ caedes/global.gd
@@ -3,3 +3,6 @@ extends Node
 var andresCurrentAttack = false
 var healthEnemy = 100
 var andresInattackZone = false
+
+var labelOK = false
+var cerrarPad = false
