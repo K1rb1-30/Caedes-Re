c9e198436857a5c1eb771c0c13fd3b1ace4913c9
aaleex05 <alexapsa2007@gmail.com>
2025-03-19 19:30:18 +0100

    timerDialogo

--- caedes/Escenarios/Nivel 0/area_2d.gd
+++ caedes/Escenarios/Nivel 0/area_2d.gd
@@ -1,9 +1,6 @@
 extends Area2D
-@onready var andres: CharacterBody2D = $JugadorPRUEBA
+@onready var andres: CharacterBody2D = $"../Andres"
 
-var ya_aparecio = false  # Variable para controlar si ya ha aparecido
 
-func _on_body_entered(body: Node2D) -> void:
-		andres = body
-		DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/Level0/Tutorial.dialogue"), "tutocat")
-		self.queue_free()
+func _on_timer_dialogo_timeout() -> void:
+	DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/Level0/Tutorial.dialogue"), "tutocat")
