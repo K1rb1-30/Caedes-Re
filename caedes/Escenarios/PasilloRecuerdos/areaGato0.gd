b4f36c624fd426cf18fdb4a6ce8680ebd237cad8
Gabriel Sánchez Flores <f1resecundario@gmail.com>
2025-03-25 16:22:44 +0100

    Añadido Zona de dialogo

---
+++ caedes/Escenarios/PasilloRecuerdos/areaGato0.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var andres: CharacterBody2D = $Jugador
+
+func _on_body_entered(body: Node2D) -> void:
+		andres = body
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/Level0/Tutorial.dialogue"), "tutocat")
+		self.queue_free()
