b1d3165344ee1ec8dc3a5ecdc7e3f20afe79afb3
aaleex05 <alexapsa2007@gmail.com>
2025-03-25 18:00:10 +0100

    mapaDecoraciones

---
+++ caedes/Escenarios/Nivel 0/camilla_00.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func playAnimationCandelario(candelario: String):
+	var sprites = get_tree().get_nodes_in_group("anim_candelario")
+	for sprite in sprites:
+		if sprite is AnimatedSprite2D:
+			sprites.play(candelario)
+	
+func _ready() -> void:
+	playAnimationCandelario("candelario")
+	
+	
