32dae88d5161e5ee641859ac0b5cf64ebfb79756
aaleex05 <alexapsa2007@gmail.com>
2025-04-08 20:27:51 +0200

    jugador

--- caedes/Escenarios/Nivel 0/camilla_00.gd
+++ caedes/Escenarios/Nivel 0/camilla_00.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@onready var andresScript: Script = load("res://assets/character/andres/characterAndresWASD.gd")
+@onready var enemyScript: Script = load("res://assets/enemies/enemy.gd")
+@onready var andres: CharacterBody2D = $Andres
+@onready var enemy: CharacterBody2D = $Enemy
+var scriptActive = false
+
+func _ready():
+		playAnimationCandelario("candelario")
+	playAnimationAntorcha("antorcha")
+	
 
 func playAnimationCandelario(candelario: String):
 	var spritesCandelario = get_tree().get_nodes_in_group("anim_candelario")
@@ -11,9 +21,6 @@ func playAnimationAntorcha(antorcha: String):
 	for spriteAn in spritesAntorcha:
 		if spriteAn is AnimatedSprite2D:
 			spriteAn.play(antorcha)
-	
-func _ready() -> void:
-	playAnimationCandelario("candelario")
-	playAnimationAntorcha("antorcha")
+
 
 	
--- caedes/Menu/menu_de_inicio.gd
+++ caedes/Menu/menu_de_inicio.gd
@@ -1,8 +1,13 @@
 extends Control
 
+var cambiarEscena = preload("res://Escenarios/Nivel 0/Camilla00.tscn")
+@onready var volumen_num: Label = $VBoxContainer2/VolumenNum
+@onready var settings: VBoxContainer = $VBoxContainer2
+@onready var menu: HBoxContainer = $VBoxContainer
+
 
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(cambiarEscena)
 
 
 func _on_exit_pressed() -> void:
@@ -10,4 +15,31 @@ func _on_exit_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	settings.visible = true
+	menu.visible = false
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value)
+	volumen_num.text = str(value)
+	
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_resolucion_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1660,990))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
+
+
+func _on_save_pressed() -> void:
+	settings.visible = false
+	menu.visible = true
+	
