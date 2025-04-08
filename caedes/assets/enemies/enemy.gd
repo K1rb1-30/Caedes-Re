9344a9455dc86c740134e4395b002a9f19b53ef2
aaleex05 <alexapsa2007@gmail.com>
2025-04-08 17:50:08 +0200

    candelario okkkkkkk

--- caedes/assets/enemies/enemy.gd
+++ caedes/assets/enemies/enemy.gd
@@ -21,7 +21,7 @@ func _on_detection_area_body_entered(body):
 		print("area dentro")
 
 
-func _on_detection_area_body_exited(body):
+func _on_detection_area_body_exited():
 	Andres = null
 	playerChase = false
 	print("area fuera")
