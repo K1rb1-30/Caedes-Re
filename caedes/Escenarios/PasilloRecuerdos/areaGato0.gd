d2c3ec5a3b935f734b2a7f4835db34327531dddb
Gabriel Sánchez Flores <f1resecundario@gmail.com>
2025-05-14 20:30:50 +0200

    añadido lineas de dialogo y areas
    tambn añadido que puedas seleccionar el archivo y el dialogo desde afuera

--- caedes/Escenarios/PasilloRecuerdos/areaGato0.gd
+++ caedes/Escenarios/PasilloRecuerdos/areaGato0.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-@onready var andres: CharacterBody2D = $Jugador
+@export var ArchivoDialogo : String
+@export var DialogoRenderizar : String
+@onready var andres: CharacterBody2D = $"../Andres"
 
 func _on_body_entered(body: Node2D) -> void:
 		andres = body
-		DialogueManager.show_example_dialogue_balloon(load("res://Dialogos/PasilloRecuerdos/Pasillo0.dialogue"), "start")
+		DialogueManager.show_example_dialogue_balloon(load(ArchivoDialogo), DialogoRenderizar)
 		self.queue_free()
