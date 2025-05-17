ec6985561a983de276eb94f48034429f3eb7ad6b
Gabriel Sánchez Flores <f1resecundario@gmail.com>
2025-05-17 11:01:24 +0200

    Finalizado los dialogos del tutorial y añadido que cuando un personaje hable aparezca su imagen

--- caedes/Dialogos/CajaDeDialogos/balloon.gd
+++ caedes/Dialogos/CajaDeDialogos/balloon.gd
@@ -45,6 +45,9 @@ var mutation_cooldown: Timer = Timer.new()
 ## The label showing the name of the currently speaking character
 @onready var character_label: RichTextLabel = %CharacterLabel
 
+## ESTO ES PARA SETEAR EL PNG DE PERSONAJE SEGUN QUIEN HABLE
+@onready var portrait: TextureRect = $Balloon/Character/TextureRect
+
 ## The label showing the currently spoken dialogue
 @onready var dialogue_label: DialogueLabel = %DialogueLabel
 
@@ -97,6 +100,11 @@ func apply_dialogue_line() -> void:
 
 	character_label.visible = not dialogue_line.character.is_empty()
 	character_label.text = tr(dialogue_line.character, "dialogue")
+	var portrait_path : String = "res://assets/PerDialogos/%s/character/0.png" % dialogue_line.character.to_lower()
+	if FileAccess.file_exists(portrait_path):
+		portrait.texture = load(portrait_path)
+	else:
+		portrait.texture = null
 
 	dialogue_label.hide()
 	dialogue_label.dialogue_line = dialogue_line
